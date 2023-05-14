5dc8d61b1d5ac3690418c4669b5c31d7a6bc56bf
Jamel-miraoui <105128037+Jamel-miraoui@users.noreply.github.com>
2023-05-14 01:22:36 +0100

    test con java

--- Advanced Java/DataBase/src/IsertWin/OracleConnection.java
+++ Advanced Java/DataBase/src/IsertWin/OracleConnection.java
@@ -6,10 +6,10 @@ import java.sql.SQLException;
 
 public class OracleConnection {
     private static Connection con;
-    public static final String URL = "jdbc:oracle:thin:@localhost:1521:ORCL";
+    public static final String URL = "jdbc:oracle:thin:@localhost:1521:XE";
     public static final String USER = "system";
     public static final String PASSWORD = "mr";
-    public static final String DRIVER_CLASS = "oracle.jdbc.driver.OracleDriver";
+    public static final String DRIVER_CLASS = "oracle.jdbc.OracleDriver";
 
     // private constructor
     private OracleConnection() {
@@ -26,6 +26,11 @@ public class OracleConnection {
         } catch (SQLException e) {
             System.out.println("ERROR: Unable to Connect to Database.");
             e.printStackTrace();
+            // Handle specific error ORA-28040: No matching authentication protocol
+            if (e.getErrorCode() == 28040) {
+                System.out.println("Please check the authentication protocol settings in the Oracle database.");
+                System.out.println("Ensure that the JDBC driver and Oracle database are compatible.");
+            }
         }
         return con;
     }
--- Advanced Java/DataBase/src/IsertWin/TestConnection.java
+++ Advanced Java/DataBase/src/IsertWin/TestConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 public class TestConnection {
     public static void main(String[] args) {
         // Get a connection from MySQLConnection class
-        Connection connection = OracleConnection.getConnection();
+        Connection connection = MySQLConnection.getConnection();
 
         if (connection != null) {
             System.out.println("Connected to MySQL database.");
